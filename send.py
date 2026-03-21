521171e80055e9b5f6d4982517e506615f1190a8
leorterl <Leorterl@gmail.com>
2026-03-21 10:24:12 +0200

    Update send.py

--- send.py
+++ send.py
@@ -1,1 +1,12 @@
+import requests
 
+BOT_TOKEN = "8370574339:AAE_X_nxsveEfou7LnT5LMQcZsWNcpHOCr4"
+CHAT_ID = "448738115"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
